fca91c3d6cfb7239c41bb8ddc48bdc4ad30a1740
marqueewinq <mark.vinogradov@phystech.edu>
2020-01-30 15:10:03 +0300

    added Token.create on User.create signal

--- pairings/pods/models.py
+++ pairings/pods/models.py
@@ -1,6 +1,10 @@
 from pprint import pformat
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class PlayerName(models.Model):
@@ -28,3 +32,17 @@ class Tournament(models.Model):
 
     def __str__(self):
         return pformat({"name": self.name, "data": self.data})
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """
+        Create Token object, when User object create
+        cobrain token is used as Token.key
+        :param sender:
+        :param instance: User instance
+        :param created: True if User object created
+        :param kwargs:
+        :return:
+    """
+    if created:
+        Token.objects.create(user=instance)
